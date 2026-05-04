b9ac8e34ad520209399d691d410ddd979a3bd8e9
lab1702 <lab1702@gmail.com>
2026-05-03 20:28:48 -0400

    Update chartSuperTrend default col to bicolor green/red
    Mirrors the addSuperTrend signature change. col is now a length-2
    character vector (uptrend, downtrend).

--- R/chartSuperTrend.R
+++ R/chartSuperTrend.R
@@ -1,7 +1,7 @@
 #' Chart a Series with SuperTrend Overlay
 #'
 #' Convenience wrapper that draws a \code{\link[quantmod]{chartSeries}}
-#' price chart and overlays the SuperTrend line in one call.
+#' price chart and overlays the bicolor SuperTrend line in one call.
 #'
 #' @param x An \code{xts} OHLC series.
 #' @param ... Additional arguments passed to
@@ -9,7 +9,8 @@
 #'   \code{subset}, \code{TA}).
 #' @param name Chart title. Defaults to the deparsed input expression.
 #' @param n,multiplier,atr_method,col Passed through to
-#'   \code{\link{addSuperTrend}}.
+#'   \code{\link{addSuperTrend}}. \code{col} is a length-2 character
+#'   vector: uptrend color, downtrend color.
 #'
 #' @return Invisibly \code{NULL}; called for the side effect of drawing.
 #'
@@ -23,7 +24,7 @@
 chartSuperTrend <- function(x, ..., name = deparse(substitute(x)),
                             n = 10, multiplier = 3,
                             atr_method = c("wilder", "sma", "ema"),
-                            col = "#1976d2") {
+                            col = c("#26a69a", "#ef5350")) {
   atr_method <- match.arg(atr_method)
   quantmod::chartSeries(x, name = name, ...)
   addSuperTrend(n = n, multiplier = multiplier,
